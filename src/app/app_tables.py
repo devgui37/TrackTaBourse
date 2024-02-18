d1c7439c06ca37929270abdfd2ef6adca1dc21b9
Guillaume DEVANT <guillaume.devant@outlook.com>
2024-02-18 12:36:41 +0100

    test

--- src/app/app_tables.py
+++ src/app/app_tables.py
@@ -59,14 +59,14 @@ def prepare_table(df_releves, df_gb):
     df_resume = df_gb.join(df_div, on="produit", how="outer")
     df_resume = df_resume.with_columns(pl.col("montant").fill_null(strategy="zero"))
     df_resume = df_resume.with_columns(
-        evolution = ((pl.col("cotation") - pl.col("PRU")) / pl.col("PRU") * 100),
-        #.alias("evolution"),
-        #(
-        #    (pl.col("cotation") - pl.col("PRU")) * pl.col("nombre")
-        #).alias("perf"),
-        #(
-        #    (pl.col("cotation") - pl.col("PRU")) * pl.col("nombre") + pl.col("montant")
-        #).alias("perf_div"),
+        ((pl.col("cotation").sub(pl.col("PRU"))).truediv(pl.col("PRU") * 100))
+        .alias("evolution"),
+        (
+            (pl.col("cotation").sub(pl.col("PRU"))) * pl.col("nombre")
+        ).alias("perf"),
+        (
+            (pl.col("cotation").sub(pl.col("PRU"))) * pl.col("nombre") + pl.col("montant")
+        ).alias("perf_div"),
     )
 
     df_resume = df_resume.with_columns(
